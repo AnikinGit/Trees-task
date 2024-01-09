09b9c19bf9d088e84494fc1f4f3c8edb5ed5c51a
”Taras <anikintar@gmail.com>
2024-01-09 15:27:01 +0200

    Initial commit

---
+++ src/app/BinaryTree.java
@@ -0,0 +1,72 @@
+package app;
+
+public class BinaryTree {
+    Node root;
+
+    public static void main(String[] args) {
+        BinaryTree tree = new BinaryTree();
+        tree.root = new Node(1);
+        tree.root.left = new Node(2);
+        tree.root.right = new Node(3);
+        tree.root.left.left = new Node(4);
+        tree.root.left.right = new Node(5);
+        tree.root.right.left = new Node(6);
+        tree.root.right.right = new Node(7);
+        tree.root.left.left.left = new Node(8);
+        tree.root.left.left.right = new Node(9);
+        tree.root.left.right.left = new Node(10);
+        tree.root.left.right.right = new Node(11);
+        tree.root.right.left.left = new Node(12);
+
+        System.out.println("Inorder traversal");
+        tree.inorder(tree.root);
+
+        System.out.println("\nPreorder traversal ");
+        tree.preorder(tree.root);
+
+        System.out.println("\nPostorder traversal");
+        tree.postorder(tree.root);
+
+
+    }
+
+    BinaryTree() {
+        root = null;
+    }
+
+    void postorder(Node node) {
+        if (node == null)
+            return;
+
+        // Траверс вліво
+        postorder(node.left);
+        // Траверс вправо
+        postorder(node.right);
+        // Траверс кореневий
+        System.out.print(node.item + " ");
+    }
+
+    void inorder(Node node) {
+        if (node == null)
+            return;
+
+        // Траверс вліво
+        inorder(node.left);
+        // Траверс кореневий
+        System.out.print(node.item + " ");
+        // Траверс вправо
+        inorder(node.right);
+    }
+    void preorder(Node node) {
+        if (node == null)
+            return;
+
+        // Траверс кореневий
+        System.out.print(node.item + " ");
+        // Траверс вліво
+        preorder(node.left);
+        // Траверс вправо
+        preorder(node.right);
+    }
+
+}
